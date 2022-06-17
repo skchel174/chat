688561e9fc4dc6adb8c7b6ec63ecb726172fc484
Evgeny Kuzmin <skchel174@gmail.com>
2022-06-17 19:20:00 +0500

    FRONT; added fake date

---
+++ frontend/src/storage/messages.js
@@ -0,0 +1,89 @@
+export default [
+  {
+    date: '2022-06-08',
+    messages: [
+      {
+        id: 1,
+        chatId: 1,
+        author: {id: 1, name: 'John Doe', img: 'john-doe-avatar.png'},
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, rerum.',
+        time: '15:03',
+      },
+      {
+        id: 2,
+        chatId: 1,
+        author: {id: 2, name: 'Homer Simpson', img: 'john-doe-avatar.png'},
+        text: 'Lorem ipsum dolor sit amet?',
+        time: '18:12',
+      },
+    ],
+  },
+
+  {
+    date: '2022-06-09',
+    messages: [
+      {
+        id: 3,
+        chatId: 1,
+        author: {id: 1, name: 'John Doe', img: 'john-doe-avatar.png'},
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis distinctio dolores, facilis laudantium repudiandae tempora vel. Accusamus alias animi dolorem ea eius, facere illo impedit itaque, laboriosam odit quia, quo quos recusandae. At autem culpa debitis dicta distinctio dolores ex facere fuga, impedit in nam officiis, quasi quo tempore voluptate! Alias commodi cupiditate deleniti minima nemo porro praesentium quas qui quidem quo! Accusantium ad amet architecto atque blanditiis corporis cupiditate delectus, dolorem dolores dolorum et ex excepturi exercitationem expedita fugit itaque iusto laboriosam modi nemo nesciunt nisi omnis pariatur perferendis provident quaerat quam quasi quia reprehenderit saepe similique sunt suscipit tempora tempore unde ut voluptas voluptates! Accusantium ad assumenda consequatur consequuntur dolor ducimus ea facilis illo ipsam ipsum itaque nesciunt nihil nisi, nulla numquam pariatur perspiciatis quas quibusdam quis quo rem repudiandae rerum saepe sapiente sit voluptatibus voluptatum! A accusamus, ad amet aspernatur commodi eaque eligendi eum, excepturi facere iusto laboriosam minima molestiae nam nobis, porro quaerat quam quia rem saepe suscipit ut velit vero voluptatibus. Asperiores, dignissimos facere? Aliquid commodi dicta dolor inventore ipsa minima omnis quaerat quos?',
+        time: '11:21',
+      },
+      {
+        id: 4,
+        chatId: 1,
+        author: {id: 1, name: 'John Doe', img: 'john-doe-avatar.png'},
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, rerum.',
+        time: '15:03',
+      },
+      {
+        id: 5,
+        chatId: 1,
+        author: {id: 2, name: 'Homer Simpson', img: 'john-doe-avatar.png'},
+        text: 'Lorem ipsum dolor sit amet? Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, rerum.',
+        time: '18:12',
+      },
+    ],
+  },
+
+  {
+    date: '2022-06-10',
+    messages: [
+      {
+        id: 6,
+        chatId: 1,
+        author: {id: 1, name: 'John Doe', img: 'john-doe-avatar.png'},
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid dolores ea earum nihil odit, officiis, optio porro repellendus similique tempore voluptatibus voluptatum? Accusantium asperiores beatae culpa doloremque iure necessitatibus numquam repudiandae tempora voluptate voluptatum?',
+        time: '18:20',
+      },
+      {
+        id: 7,
+        chatId: 1,
+        author: {id: 1, name: 'John Doe', img: 'john-doe-avatar.png'},
+        text: 'Lorem ipsum dolor sit amet? Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, rerum.',
+        time: '18:48',
+      },
+      {
+        id: 8,
+        chatId: 1,
+        author: {id: 2, name: 'Homer Simpson', img: 'john-doe-avatar.png'},
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, quasi, sit. Alias ex excepturi expedita hic vel? Adipisci asperiores consequuntur dolore dolorum, eius eligendi est, explicabo fugit, illo ipsam itaque labore laborum necessitatibus nesciunt odio officiis porro possimus qui quisquam quod recusandae repellendus suscipit ut vel veniam vitae voluptate!',
+        time: '18:48',
+      },
+      {
+        id: 9,
+        chatId: 1,
+        author: {id: 1, name: 'John Doe', img: 'john-doe-avatar.png'},
+        text: 'Lorem ipsum dolor sit amet?',
+        time: '19:05',
+      },
+      {
+        id: 10,
+        chatId: 1,
+        author: {id: 2, name: 'Homer Simpson', img: 'john-doe-avatar.png'},
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, quasi, sit. Alias ex excepturi expedita hic vel?',
+        time: '20:01',
+      },
+    ],
+  },
+];

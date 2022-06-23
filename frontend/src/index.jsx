f5bf14e5bf6efce9d020023bef32caadcfdcccb5
Evgeny Kuzmin <skchel174@gmail.com>
2022-06-23 16:09:07 +0500

    FRONT; updated index.js, added redux store provider

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import {Provider} from "react-redux";
+import store from "./store";
+import {BrowserRouter} from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

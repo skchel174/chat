50fe21e2c9e6a0ab20c3c7ca4da165afb73f8aae
Evgeny Kuzmin <skchel174@gmail.com>
2022-06-24 22:23:48 +0500

    FRONT; added settings components to ComponentResolver

--- frontend/src/infrastructure/Context/ComponentResolver.jsx
+++ frontend/src/infrastructure/Context/ComponentResolver.jsx
@@ -1,14 +1,18 @@
+import PropTypes from "prop-types";
 import Chats from "components/Chats";
 import Search from "components/Search";
 import Profile from "components/Profile";
 import Dialog from "components/Dialog";
-import PropTypes from "prop-types";
+import {SettingsMenu, ProfileSettings, GeneralSettings} from "components/Settings";
 
 export const components = {
   Dialog,
   Chats,
   Search,
   Profile,
+  SettingsMenu,
+  ProfileSettings,
+  GeneralSettings,
 };
 
 const ComponentResolver = ({is}) => {

d04e6549a65fdcdae6c6eb4d514e359a18520411
Evgeny Kuzmin <skchel174@gmail.com>
2022-07-11 16:49:05 +0500

    FRONT; removed Root from App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
-import {styled} from "@mui/material";
 import ThemeProvider from "theme/ThemeProvider";
 import Router from "router";
 import Auth from "auth";
-
-const Root = styled("div")(
-  () => ({
-    width: "100%",
-    height: "100%",
-  })
-);
 
 const App = () => {
   return (
     <ThemeProvider>
       <Auth>
-        <Root>
-          <Router/>
-        </Root>
+        <Router/>
       </Auth>
     </ThemeProvider>
   );

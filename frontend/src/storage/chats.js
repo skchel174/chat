475ae30aee88e5bd5b5dd15a8d058ce3124d7787
Evgeny Kuzmin <skchel174@gmail.com>
2022-07-15 15:24:50 +0500

    FRONT; updated fake data, removed date and time props from messages

--- frontend/src/storage/chats.js
+++ frontend/src/storage/chats.js
@@ -16,9 +16,7 @@ export default [
       chatId: 1,
       authorId: 1,
       text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis distinctio dolores, facilis laudantium repudiandae tempora vel. Accusamus alias animi dolorem ea eius, facere illo impedit itaque, laboriosam odit quia, quo quos recusandae. At autem culpa debitis dicta distinctio dolores ex facere fuga, impedit in nam officiis, quasi quo tempore voluptate! Alias commodi cupiditate deleniti minima nemo porro praesentium quas qui quidem quo! Accusantium ad amet architecto atque blanditiis corporis cupiditate delectus, dolorem dolores dolorum et ex excepturi exercitationem expedita fugit itaque iusto laboriosam modi nemo nesciunt nisi omnis pariatur perferendis provident quaerat quam quasi quia reprehenderit saepe similique sunt suscipit tempora tempore unde ut voluptas voluptates! Accusantium ad assumenda consequatur consequuntur dolor ducimus ea facilis illo ipsam ipsum itaque nesciunt nihil nisi, nulla numquam pariatur perspiciatis quas quibusdam quis quo rem repudiandae rerum saepe sapiente sit voluptatibus voluptatum! A accusamus, ad amet aspernatur commodi eaque eligendi eum, excepturi facere iusto laboriosam minima molestiae nam nobis, porro quaerat quam quia rem saepe suscipit ut velit vero voluptatibus. Asperiores, dignissimos facere? Aliquid commodi dicta dolor inventore ipsa minima omnis quaerat quos?',
-      date: '2022-06-09',
-      time: '11:21',
-      datetime: '2022-06-08T15:03',
+      datetime: '2022-06-08T19:13',
     },
   },
 
@@ -39,9 +37,7 @@ export default [
       chatId: 2,
       authorId: 1,
       text: 'Aspernatur culpa fuga hic nam praesentium!',
-      date: '2022-06-12',
-      time: '11:22',
-      datetime: '2022-06-08T15:03',
+      datetime: '2022-07-15T14:39',
     },
   },
 

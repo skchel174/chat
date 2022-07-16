8d0575fca5f264861aba8ef1e0119470f58c4ea6
Evgeny Kuzmin <skchel174@gmail.com>
2022-07-16 15:27:40 +0500

    WS; created websocket service

---
+++ ws/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(
+  3000,
+  () => console.log('Websocket server started...')
+);
